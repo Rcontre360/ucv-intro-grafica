34de7039abca5fdde120fcbe1bc518411dc83011
Rcontre360 <rcontreraspimentel@gmail.com>
2025-11-05 21:13:32 -0400

    fixed bug on coordenades when updating pixel. made simple linnes

--- tarea-1/src/main.cpp
+++ tarea-1/src/main.cpp
@@ -1,23 +1,83 @@
 #include "PixelRender.h"
 #include <iostream>
+#include <cmath>
+// std::pair
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+using LINE = pair<pair<int,int>, pair<int,int>>;
 
 class CMyTest : public CPixelRender
 {
+protected:
+    float line_color[3] = {1.0f, 0.5f, 0.0f};
+    bool use_bresenham = true;
+    int frames_by_second = 0;
+    int m_x0 = -1, m_y0 = -1, m_x1 = -1, m_y1 = -1;
+
+    vector<LINE> lines;
+
 public:
     CMyTest() {};
     ~CMyTest() {};
 
+    void drawInterface()
+    {
+        double currentTime = glfwGetTime();
+        double deltaTime = currentTime - lastTime;
+
+        frames_by_second+=1;
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::SetNextWindowSize(ImVec2(350, 200), ImGuiCond_Once); // Adjusted size for new controls
+        ImGui::Begin("Control Panel");
+        ImGui::SetWindowFontScale(1.5f);
+
+        ImGui::SliderFloat("R", &line_color[0], 0.0f, 1.0f);
+        ImGui::SliderFloat("G", &line_color[1], 0.0f, 1.0f);
+        ImGui::SliderFloat("B", &line_color[2], 0.0f, 1.0f);
+
+        if (ImGui::Checkbox("Use Bresenham", &use_bresenham)) {
+            printf("Wireframe toggled: %s\n", use_bresenham ? "true" : "false");
+        }
+
+        if (ImGui::Button("Generate random", ImVec2(200,35))) {
+            printf("Button 'Recalculate Data' pressed!\n");
+        }
+        
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (deltaTime >= 1.0) {
+            char title[256];
+            snprintf(title, sizeof(title), "frames per second: %.2lf", frames_by_second / deltaTime);
+            glfwSetWindowTitle(m_window, title);
+            lastTime = currentTime;
+            frames_by_second = 0;
+        }
+    }
+
     void update()
     {
         std::fill(m_buffer.begin(), m_buffer.end(), RGBA{ 0,0,0,0 });
-        pixelsModifiedThisSecond += m_nPixels;
-        // tu código actual para modificar pixels...
-        for (int i = 0; i < m_nPixels; ++i)
+        if ((mouseButtonsDown[0] || mouseButtonsDown[1] || mouseButtonsDown[2]) && m_x1 > -1 && m_y1 > -1)
         {
-            int x = rand() % width;
-            int y = rand() % height;
-            RGBA color = { (unsigned char)(rand() % 256), (unsigned char)(rand() % 256), (unsigned char)(rand() % 256), 255 };
-            setPixel(x, y, color);
+            int den = (m_x1 - m_x0);
+            float m = den==0? 0 : (float)(m_y1 - m_y0) / (float)den;
+            float b = (float)m_y0 - m*(float)m_x0;
+
+            for (int i = m_x0; i < m_x1; ++i)
+            {
+                int y = (int)std::round(m*(float)i+b);
+                RGBA color = { (unsigned char)(255), (unsigned char)(255), (unsigned char)(255), 255 };
+                setPixel(i, y, color);
+            }
         }
     }
 
@@ -43,13 +103,17 @@ public:
             if (action == GLFW_PRESS)
             {
                 mouseButtonsDown[button] = true;
-                // Obtener posición actual del cursor
+                m_x0 = xpos;
+                m_y0 = ypos;
                 std::cout << "Mouse button " << button << " pressed at position (" << xpos << ", " << ypos << ")\n";
             }
             else if (action == GLFW_RELEASE)
             {
 
                 mouseButtonsDown[button] = false;
+                LINE cur_line = { {m_x0,m_y0}, {m_x1,m_y1} };
+                m_x1, m_y1 = -1;
+                lines.push_back(cur_line);
                 std::cout << "Mouse button " << button << " released at position (" << xpos << ", " << ypos << ")\n";
             }
         }
@@ -59,7 +123,8 @@ public:
     {
         if (mouseButtonsDown[0] || mouseButtonsDown[1] || mouseButtonsDown[2]) 
         {
-            std::cout << "Mouse move at (" << xpos << ", " << ypos << ")\n";
+            m_x1 = xpos;
+            m_y1 = ypos;
         }
     }
 };
