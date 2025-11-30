edef9e2a9ef8d64e00120a9803836733b5fa4ddd
Rcontre360 <rcontreraspimentel@gmail.com>
2025-11-30 15:05:01 -0400

    fixed double draw on triangle

--- proyecto-1/src/primitives/triangle.rs
+++ proyecto-1/src/primitives/triangle.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::line::draw_line;
 use crate::canvas::Canvas;
 use crate::core::{Point, RGBA, ShapeCore, ShapeImpl};
@@ -52,35 +54,31 @@ fn draw_triangle(core: &ShapeCore, canvas: &mut Canvas) {
     if core.points.len() <= 2 {
         draw_line(&core, canvas, false);
     } else {
+        // we dont store the pixel cache on the triangle because its data depends on the triangle
+        // control points and if we modify them we end up needing another pixel cache
+        let mut pixel_cache = HashMap::new();
+        let ab = core.points[0..2].to_vec();
+        let bc = core.points[1..3].to_vec();
+        let ca = vec![core.points[2], core.points[0]];
+
+        draw_line_for_triangle(&core.copy_with_points(ab), canvas, &mut pixel_cache);
+        draw_line_for_triangle(&core.copy_with_points(bc), canvas, &mut pixel_cache);
+        draw_line_for_triangle(&core.copy_with_points(ca), canvas, &mut pixel_cache);
+
         // if we have a fill color defined then we will fill the triangle
         if !core.fill_color.is_transparent() {
-            fill_triangle(core, canvas);
+            fill_triangle(core, canvas, &pixel_cache);
         }
 
-        // here is important to notice that we should not draw 2 times the same pixel (endings of
-        // lines).
-        // So we do a trick, first line drawn is (a,b], second is (c,a] and third one is (b,c]
-        let ab = core.points[0..2].to_vec();
-        let ca = vec![core.points[2], core.points[0]];
-        let bc = core.points[1..3].to_vec();
-
-        let ab_core = core.copy_with_points(ab);
-        let ca_core = core.copy_with_points(ca);
-        let bc_core = core.copy_with_points(bc);
-
-        // draws b but not a
-        draw_line(&ab_core, canvas, false);
-        // draws a but not c
-        draw_line(&ca_core, canvas, false);
-        // draws c but not b
-        draw_line(&bc_core, canvas, false);
+        // pixel cache content is released from memory here thanks to the magic of ownership and
+        // borrowing in rust: https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#memory-and-allocation
     }
 }
 
 /// this function fills the triangle given the shape core.
 /// to fill it we build the top triangle from the horizontal line on the corner. Then do the same
 /// on the bottom triangle
-fn fill_triangle(core: &ShapeCore, canvas: &mut Canvas) {
+fn fill_triangle(core: &ShapeCore, canvas: &mut Canvas, drawn: &HashMap<(i32, i32), bool>) {
     let mut points = core.points.clone();
     // we sort the points by "y" to get the lowest one first
     points.sort_by(|a, b| a.1.cmp(&b.1));
@@ -100,12 +98,19 @@ fn fill_triangle(core: &ShapeCore, canvas: &mut Canvas) {
     )
         .into();
     // first fills the top triangle then the bottom one
-    fill_top_triangle(canvas, p1, p2, p4, core.fill_color);
-    fill_bottom_triangle(canvas, p2, p4, p3, core.fill_color);
+    fill_top_triangle(p1, p2, p4, core.fill_color, canvas, drawn);
+    fill_bottom_triangle(p2, p4, p3, core.fill_color, canvas, drawn);
 }
 
 /// given a triangle (3 points), with the bottom side horizontal, we fill it with a color
-fn fill_top_triangle(canvas: &mut Canvas, p1: Point, p2: Point, p3: Point, color: RGBA) {
+fn fill_top_triangle(
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    color: RGBA,
+    canvas: &mut Canvas,
+    drawn: &HashMap<(i32, i32), bool>,
+) {
     let m_1 = (p2.0 - p1.0) as f32 / (p2.1 - p1.1) as f32;
     let m_2 = (p3.0 - p1.0) as f32 / (p3.1 - p1.1) as f32;
 
@@ -113,13 +118,27 @@ fn fill_top_triangle(canvas: &mut Canvas, p1: Point, p2: Point, p3: Point, color
     let mut cur_x2 = p1.0 as f32;
 
     for y in p1.1..p2.1 {
-        draw_horizontal(canvas, cur_x1.ceil() as i32, cur_x2.ceil() as i32, y, color);
+        draw_horizontal(
+            cur_x1.ceil() as i32,
+            cur_x2.ceil() as i32,
+            y,
+            color,
+            canvas,
+            drawn,
+        );
         cur_x1 += m_1;
         cur_x2 += m_2;
     }
 }
 
-fn fill_bottom_triangle(canvas: &mut Canvas, p1: Point, p2: Point, p3: Point, color: RGBA) {
+fn fill_bottom_triangle(
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    color: RGBA,
+    canvas: &mut Canvas,
+    drawn: &HashMap<(i32, i32), bool>,
+) {
     let m_1 = (p3.0 - p1.0) as f32 / (p3.1 - p1.1) as f32;
     let m_2 = (p3.0 - p2.0) as f32 / (p3.1 - p2.1) as f32;
 
@@ -128,23 +147,33 @@ fn fill_bottom_triangle(canvas: &mut Canvas, p1: Point, p2: Point, p3: Point, co
 
     for y in (p1.1..p3.1).rev() {
         draw_horizontal(
-            canvas,
             cur_x1.floor() as i32,
             cur_x2.floor() as i32,
             y,
             color,
+            canvas,
+            drawn,
         );
         cur_x1 -= m_1;
         cur_x2 -= m_2;
     }
 }
 
-fn draw_horizontal(canvas: &mut Canvas, mut x1: i32, mut x2: i32, y: i32, color: RGBA) {
+fn draw_horizontal(
+    mut x1: i32,
+    mut x2: i32,
+    y: i32,
+    color: RGBA,
+    canvas: &mut Canvas,
+    drawn: &HashMap<(i32, i32), bool>,
+) {
     if x1 > x2 {
         std::mem::swap(&mut x1, &mut x2);
     }
     for x in x1..x2 {
-        canvas.set_pixel(x, y, color);
+        if drawn.get(&(x, y)).is_none() {
+            canvas.set_pixel(x, y, color);
+        }
     }
 }
 
@@ -163,4 +192,85 @@ fn edge_side_check(a: Point, b: Point, p: Point) -> i64 {
     let ap_y = py - ay;
 
     ab_x * ap_y - ab_y * ap_x
+}
+
+/// same algorithm used in draw_line with the DIFFERENCE that this one tracks every pixel of the
+/// borders. since there's not an effective way to avoid drawing the same pixel twice with
+/// triangles with very narrow angles (45 degrees or less) we do something different for this shape
+/// we will track all border pixels, this way we wont have issues with drawing the same
+/// pixel twice
+/// we copied the algorithm instead of making the modification on the other one BECAUSE we dont
+/// want the performance of others to be hit by this one, also only the triangle suffers from this
+/// error.
+pub fn draw_line_for_triangle<'a>(
+    core: &ShapeCore,
+    canvas: &mut Canvas<'a>,
+    drawn: &mut HashMap<(i32, i32), bool>,
+) {
+    let points = &core.points;
+    let a = points[0];
+    let b = points[1];
+
+    let mut dx = (b.0 - a.0) as i32;
+    let mut dy = (b.1 - a.1) as i32;
+    let x_inc = if dx < 0 { -1 as i32 } else { 1 };
+    let y_inc = if dy < 0 { -1 as i32 } else { 1 };
+
+    if dx < 0 {
+        dx *= -1;
+    }
+    if dy < 0 {
+        dy *= -1;
+    }
+
+    let run_on_x = dx >= dy;
+    let inc_e = -2 * (if run_on_x { dy } else { dx });
+    let inc_ne = 2 * (dx - dy) * (if run_on_x { 1 } else { -1 });
+
+    let mut d = (dx - 2 * dy) * if run_on_x { 1 } else { -1 };
+    let mut x = a.0 as i32;
+    let mut y = a.1 as i32;
+
+    let mut draw_if_available = |x, y| {
+        if drawn.get(&(x, y)).is_none() {
+            drawn.insert((x, y), true);
+            canvas.set_pixel(x, y, core.color);
+        }
+    };
+
+    draw_if_available(x, y);
+
+    if run_on_x {
+        while x > b.0 || x < b.0 {
+            if d <= 0 {
+                d += inc_ne;
+                y += y_inc;
+            } else {
+                d += inc_e;
+            }
+
+            x += x_inc;
+
+            draw_if_available(x, y);
+        }
+    } else {
+        while (y > b.1 || y < b.1) {
+            if d <= 0 {
+                d += inc_ne;
+                x += x_inc;
+            } else {
+                d += inc_e;
+            }
+
+            y += y_inc;
+
+            draw_if_available(x, y);
+        }
+
+        x += x_inc;
+        // edge case found. we want to draw a full line from a to b inclusive
+        // this ensures that b is drawn, when reaching this else condition it is not drawn
+        // This was tested to check if there are overlaps on the canvas
+        draw_if_available(x, y);
+    }
 }
