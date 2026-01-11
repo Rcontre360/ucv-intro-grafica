3090bbc2167459f7d1c473dcca3c40d383837df9
Rcontre360 <rcontreraspimentel@gmail.com>
2026-01-10 21:09:36 -0400

    finished

--- tarea-3/src/median.h
+++ tarea-3/src/median.h
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// moves all elements bigger than arr[r] to the right and lower than arr[r] to the left
 int partition(vector<unsigned char>& arr, int l, int r) {
     int x = arr[r], i = l;
     for (int j = l; j <= r - 1; j++) {
@@ -23,6 +24,7 @@ int partition(vector<unsigned char>& arr, int l, int r) {
 }
 
 // quick select recursive from https://www.geeksforgeeks.org/dsa/quickselect-algorithm/
+// not used, just shown here as reference for the iterative version
 int quick_select(vector<unsigned char>& arr, int l, int r, int k) {
     if (k > 0 && k <= r - l + 1) {
         int index = partition(arr, l, r);
@@ -64,49 +66,54 @@ unsigned char median(vector<unsigned char>& vec) {
 }
 
 class Median {
+private:
+    int kernel_size;
+
 public:
-    // Constructor validates and sets the kernel size
+    // constructor validates and sets the kernel size
     Median(int size) : kernel_size(size) {
         if (size <= 0 || size % 2 == 0) {
             throw invalid_argument("Kernel size must be a positive, odd integer.");
         }
     }
 
-    // Applies the median filter to an image and returns a new image
+    // applies the median filter to an image and returns a new image
     Image apply(const Image& input_image) const {
+        // create new image
         Image out(input_image.width(), input_image.height(), input_image.channels());
-        int kernel_half = kernel_size / 2;
+        int mid = kernel_size / 2;
 
         for (int y = 0; y < input_image.height(); y++) {
             for (int x = 0; x < input_image.width(); x++) {
                 for (int c = 0; c < input_image.channels(); c++) {
-                    if (c == 3) { // we just copy alpha if any
+                    // we just copy alpha if any
+                    if (c == 3) { 
                         unsigned char value = input_image.get(x, y, c);
                         out.set(x, y, c, value);
                         continue;
                     }
 
+                    // we run over the kernel window on the image
                     vector<unsigned char> all;
-                    for (int ky = -kernel_half; ky <= kernel_half; ky++) {
-                        for (int kx = -kernel_half; kx <= kernel_half; kx++) {
+                    for (int ky = -mid; ky <= mid; ky++) {
+                        for (int kx = -mid; kx <= mid; kx++) {
                             int pixel_y = y + ky;
                             int pixel_x = x + kx;
 
+                            // if the index is valid, we add it to the kernel window set of elements
                             if (pixel_y >= 0 && pixel_y < input_image.height() &&
                                 pixel_x >= 0 && pixel_x < input_image.width()) {
                                 all.push_back(input_image.get(pixel_x, pixel_y, c));
                             }
                         }
                     }
+                    // we set the value on that pixel with the median
                     out.set(x, y, c, median(all));
                 }
             }
         }
         return out;
     }
-
-private:
-    int kernel_size;
 };
 
 #endif 
